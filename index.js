f7f17d384b341005dcc8d4e25ceff03248b3d282
DA-WN2 <adilshancp8943@gmail.com>
2026-03-06 23:49:10 +0530

    changes in index.js commit

--- index.js
+++ index.js
@@ -41,8 +41,13 @@ app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html")),
+  app.get(
+    "/*",
+    (
+      req,
+      res, // Adding the / fixed the name parameter error
+    ) =>
+      res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html")),
   );
 } else {
   app.get("/", (req, res) => {
